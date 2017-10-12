8c18ca1ca6545a6feb29d8b30f9d0f2f172b7721
Mary Benitez <meebenitez@gmail.com>
2017-10-12 05:24:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       item_name = req.path.split("/items/").last
       if @@items.include?(item_name)
         item = @@items.find{|i| i.name = item_name}
-        resp.write "item.price"
+        resp.write "#{item.price}"
       else
         resp.write "Route not found"
         resp.status = 404
