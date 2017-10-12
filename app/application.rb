d4ca28f72cd58ada57d8ce4ef7cac2b27c86f7e8
Mary Benitez <meebenitez@gmail.com>
2017-10-12 05:44:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -13,11 +13,11 @@ class Application
         resp.write "#{item.price}"
       else
         resp.write "Item not found"
-        resp.status = 404
+        resp.status = 400
       end
     else
       resp.write "Route not found"
-      resp.status = 404
+      resp.status = 400
     end
 
     resp.finish
