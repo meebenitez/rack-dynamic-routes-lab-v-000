73065f98795ffab6c44c37baaba48312454bf037
Mary Benitez <meebenitez@gmail.com>
2017-10-12 05:09:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,8 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path=="/items/"
+      user_item = req.params["q"]
+      if @@items.include?(user_item)
       resp.write "You requested the songs"
     else
       resp.write "Route not found"
