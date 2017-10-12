20c8a8920dbf7590f1879a4270f59998fe6669c3
Mary Benitez <meebenitez@gmail.com>
2017-10-12 05:48:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,6 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
-      binding.pry
       if @@items.include?(item)
         resp.write "#{item.price}"
       else
